e118790b52ae3285f69b493b7b1cb38ae7cab321
Andrew Nelson <andyfaff@users.noreply.github.com>
2010-09-07 05:50:19 +0000

    matrix error estimation

--- errorEstimation.h
+++
@@ -1,9 +0,0 @@
-
-
-/*
- functions contained in errorEstimation.c
- */
-int getCovarianceMatrix();
-static int choldc (double **a, int N, double *p);
-static void cholsl(double **a, int N, const double *p, double *b, double *x);
-
